8c23b9156f83f15989a3f51e0714c5db056f2c9f
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2025-04-04 11:30:28 +0800

    feat(components): add HttpMethodBadge

---
+++ components/HttpMethodBadge.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+export type HttpMethodBadge = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+
+const { label } = defineProps<{ label: HttpMethodBadge }>();
+
+const color = computed(() => {
+  let $color = '';
+
+  if (label === 'GET') $color = 'success';
+  else if (label === 'POST') $color = 'warning';
+  else if (label === 'PUT') $color = 'info';
+  else if (label === 'PATCH') $color = 'neutral';
+  else $color = 'error';
+
+  return $color as
+    | 'primary'
+    | 'secondary'
+    | 'success'
+    | 'info'
+    | 'warning'
+    | 'error'
+    | 'neutral'
+    | undefined;
+});
+</script>
+
+<template>
+  <UBadge
+    :color
+    :label
+    size="sm"
+    class="w-max select-none"
+    variant="subtle"
+  />
+</template>
